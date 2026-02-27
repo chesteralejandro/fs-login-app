544efcc7b52e8631016759a9c048677e212b324d
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-02-28 01:07:40 +0800

    feat: implement user registration process

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql2/promise');
 
 class Database {
+	constructor() {
+		this.connection;
+	}
+
 	async connect() {
 		try {
-			await mysql.createConnection({
+			this.connection = await mysql.createConnection({
 				host: process.env.DATABASE_HOST,
 				user: process.env.DATABASE_USER,
 				password: process.env.DATABASE_PASSWORD,
@@ -14,6 +18,25 @@ class Database {
 		} catch (error) {
 			throw new Error(error.message);
 		}
+	}
+
+	async findUserByEmail(userEmail) {
+		const query = 'SELECT id FROM users WHERE email = ?';
+		const [userArray] = await this.connection.execute(query, [userEmail]);
+
+		return userArray;
+	}
+
+	async createUser(userData) {
+		const query =
+			'INSERT INTO `users`(`first_name`, `last_name`, `email`, `password`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, NOW(), NOW())';
+
+		const { firstName, lastName, email, password } = userData;
+		const values = [firstName, lastName, email, password];
+
+		const [result, fields] = await this.connection.execute(query, values);
+
+		console.log(result, fields);
 	}
 }
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,28 @@
+const database = require('../config/database');
+const { createHashedPassword } = require('../utils/bcrypt');
+
 class UserController {
 	processLogin(req, res) {
 		const body = req.body;
 		console.log(body);
 	}
 
-	processRegistration(req, res) {
-		const body = req.body;
-		console.log(body);
+	async processRegistration(req, res) {
+		const user = req.body;
+		try {
+			const userFound = await database.findUserByEmail(user.email);
+
+			if (Boolean(userFound.length)) {
+				throw new Error('Email already registered');
+			}
+
+			const hashedPassword = await createHashedPassword(user.password);
+
+			await database.createUser({ ...user, password: hashedPassword });
+		} catch (error) {
+			console.error('❌', error.message);
+			res.render('register-page', { error: error.message });
+		}
 	}
 
 	showLoginPage(req, res) {
