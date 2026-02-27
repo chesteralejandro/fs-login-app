f5a6fad3e633574e5d93dde6ee9e80d2c936e3f0
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-02-27 19:20:12 +0800

    refactor: move router in it's own file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 
 const server = require('./config/server');
+const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Server is in another file.');
-});
+app.use('/', userRoutes);
 
 server.listen(app);
