c8b0b5caeb3774d6da94c4b9d547337c516f67f2
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-02-27 21:48:04 +0800

    feat: create login and register pages

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,14 @@ class UserController {
 	showHomepage(req, res) {
 		res.render('homepage');
 	}
+
+	showLoginPage(req, res) {
+		res.render('login-page');
+	}
+
+	showRegisterPage(req, res) {
+		res.render('register-page');
+	}
 }
 
 module.exports = new UserController();
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/', userController.showHomepage);
+router.get('/login', userController.showLoginPage);
+router.get('/register', userController.showRegisterPage);
 
 module.exports = router;
